1500289ab66997852824e3396a87ca537a053f3b
Stephan <stephan.schueritz@gmail.com>
2021-01-19 21:03:55 +0100

    Update BoardTile - Add Callback for Movement

--- Assets/Scripts/Game/BoardTile.cs
+++ Assets/Scripts/Game/BoardTile.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.Events;
 
 namespace MAG.Game
 {
     public class BoardTile : MonoBehaviour
     {
+		public int id;
         public Vector2 size;
 		public SpriteRenderer overlayRenderer;
 
@@ -15,6 +17,7 @@ namespace MAG.Game
 		private bool selected = false;
 		private bool moving = false;
 
+		private UnityAction moveCallback;
 		public bool IsMoving => moving;
 
 		public Vector3 Position => transform.position;
@@ -31,14 +34,19 @@ namespace MAG.Game
 			overlayRenderer.color = new Color(0f, 0f, 0f, 0f);
 		}
 
-		public void SetPosition(Vector3 position)
+		public void SetPosition(Vector3 position, UnityAction callback = null)
         {
+			moveCallback = callback;
 			transform.DOMove(position, duration).OnComplete(SetPositionDone);
         }
 
 		private void SetPositionDone()
         {
-
-        }
+			if(moveCallback != null)
+            {
+				moveCallback.Invoke();
+				moveCallback = null;
+			}
+		}
 	}
 }
